d4fa308f93f73c531b30ffc92d68edea385d74a7
Eahiya <eahiamondal@gmail.com>
2020-08-17 16:02:11 +0600

    setup laravel passport with scope and done auth system to backend

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function(){
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::get('logout', 'AuthController@logout');
+    });
+
 });
 
 Route::apiResource('categories', 'CategoryController');
-
 Route::get('test', 'CategoryController@test');
